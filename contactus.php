519ec26690ab6161a8c789585766bf942ebd75d9
sumit6037 <39862602+sumit6037@users.noreply.github.com>
2018-06-05 11:12:50 +0530

    Add files via upload
    thanks

---
+++ contactus.php
@@ -0,0 +1,44 @@
+<HTML>
+<head>
+<style>
+body {
+    background-image:url('5.jpeg');
+	background-size:cover;
+}
+
+div {
+    background:rgba(200,200,200,0.5) ;
+    width: 500px;
+    border: 5px solid black;
+    padding: 25px;
+    margin: 25px;
+	align: center;
+}
+</style>
+</head>
+<title>
+Contact Us
+</title>
+<body>
+<center>
+<div>
+<h1><font size='7'><center>CONTACT US</center></FONT></h1>
+<p><font size='4'>Customer Care No. : 011-39340000, 011-23340000 (language : Hindi and English)
+Fax no. : 011-23345400.<br>
+For Railway tickets booked through RRCL<P><br>
+I-tickets/e-tickets : <font color='red'><u>care@rrcl.co.in</u></font><br>
+For Cancellation E-tickets : <font color='red'><u>etickets@rrcl.co.in</u></font><br>
+For Mumbai Suburban Season tickets : <font color='red'><u>seasontickets@rrcl.co.in</u></font> <br>
+For IRCTC SBI Card users who do not receive the card within 01 month<br>
+ from the date of application kindly call on the Railway SBI Card Helpline<br>
+ nos. at 0124-39021212 or 18001801295 (if calling from BSNL/MTNL line)<br>
+ or send email to feedback.gesbi@ge.com. For other queries on your IRCTC<br>
+ SBI card account, kindly email at loyaltyprogram@irctc.co.in <br>
+ Registered Office / Corporate Office : <br>
+Indian Railway Catering and Tourism Corporation Ltd.,<br>
+B-148, 11th Floor,Statesman House,<br>
+Barakhamba Road, New Delhi 110001.<br></font>
+</div>
+</p>
+</body>
+</html>
